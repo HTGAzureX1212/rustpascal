122ca2f3d5706109f869c5e25af2dbfdcc1eade6
HTG-YT <39023054+HTG-YT@users.noreply.github.com>
2021-04-24 11:48:27 +0800

    Update tests.rs

--- compiler/libpascalc_lexer/src/tests.rs
+++ compiler/libpascalc_lexer/src/tests.rs
@@ -1,7 +1,4 @@
-use expect_test::{
-    expect,
-    Expect
-};
+use expect_test::expect;
 
 use crate::lexical_analyze;
 
@@ -26,6 +23,40 @@ fn hello_world_test() {
     Lexeme { type: Whitespace, len: 2 }
     Lexeme { type: Identifier, len: 3 }
     Lexeme { type: Dot, len: 1 }
+    "]];
+    let actual = lexical_analyze(source).map(|lexeme| {
+        format!("{:?}\n", lexeme)
+    }).collect::<String>();
+
+    expect.assert_eq(&actual);
+}
+
+#[test]
+fn comments_test() {
+    let source = include_str!("../../tests/lexer/lexer_comment_test.pas");
+
+    let expect = expect![[r"
+    Lexeme { type: LineComment, len: 16 }
+    Lexeme { type: Whitespace, len: 1 }
+    Lexeme { type: BlockComment, len: 31 }
+    Lexeme { type: Whitespace, len: 2 }
+    Lexeme { type: BlockComment, len: 23 }
+    Lexeme { type: Whitespace, len: 1 }
+    Lexeme { type: BlockComment, len: 46 }
+    Lexeme { type: Whitespace, len: 1 }
+    "]];
+    let actual = lexical_analyze(source).map(|lexeme| {
+        format!("{:?}\n", lexeme)
+    }).collect::<String>();
+
+    expect.assert_eq(&actual);
+}
+
+#[test]
+fn integers_test() {
+    let source = include_str!("../../tests/lexer/lexer_integers_test.pas");
+
+    let expect = expect![[r"
     "]];
     let actual = lexical_analyze(source).map(|lexeme| {
         format!("{:?}\n", lexeme)
