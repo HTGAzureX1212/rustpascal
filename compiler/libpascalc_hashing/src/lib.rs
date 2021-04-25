d87a69f25e8a9850c38493f134a5a12968b5a63b
HTG-YT <39023054+HTG-YT@users.noreply.github.com>
2021-04-25 11:07:02 +0800

    Update lib.rs

--- compiler/libpascalc_hashing/src/lib.rs
+++ compiler/libpascalc_hashing/src/lib.rs
@@ -1,3 +1,30 @@
 //! `libpascalc_hashing`
 //!
 //! Custom hash algorithm implemented for the RustPascal compiler.
+
+use std::{
+    collections::{
+        HashMap,
+        HashSet
+    },
+    hash::{
+        BuildHasherDefault,
+        Hasher
+    }
+};
+
+#[cfg(target_pointer_width = "32")]
+const K: usize = 0x9e3779b9;
+#[cfg(target_pointer_width = "64")]
+const K: usize = 0x517cc1b727220a95;
+
+pub struct PascalcHasher {
+    hash: usize
+}
+
+impl Default for PascalcHasher {
+    #[inline]
+    fn default() -> Self {
+        Self { hash: 0 }
+    }
+}
